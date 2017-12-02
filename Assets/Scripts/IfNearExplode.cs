9ef57a6cea52baadde663e1d731339b2afb16f81
Bart <bartek.durys@gmail.com>
2017-12-02 23:31:38 +0300

    Some stuff

---
+++ Assets/Scripts/IfNearExplode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfNearExplode : MonoBehaviour
+{
+    public GameObject Ship;
+    public GameObject Explosives;
+    public float range;
+    // Use this for initialization
+    void Start()
+    {
+        Ship = GameObject.FindGameObjectWithTag("Ship");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Ship.transform.position, transform.position) < range)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Explosives.SetActive(true);
+        StartCoroutine(Destruction());
+    }
+
+    IEnumerator Destruction()
+    {
+        float time = 0.0f;
+        while (time < 1.0f)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public float highOffset;
+    public GameObject Teren;
+    public GameObject Ting;
+    private Terrain ter;
+    int size = 3;
+    public int differ = 10; 
+    // Use this for initialization
+	void Start () {
+        Teren = GameObject.FindGameObjectWithTag("Dno");
+        ter = Teren.GetComponent<Terrain>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
