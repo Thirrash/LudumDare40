37655bd34eabc1f8ecc4bc2b5b2b317746e0a8df
Latawiec <lmarzec08@gmail.com>
2017-12-02 23:52:43 +0100

    niewiem

---
+++ Assets/Lukasz/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 EndVector;
+    public float speed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, EndVector, speed);
+    }
+}
