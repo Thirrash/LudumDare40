a4d7149dbe5a15d1d09e0a46c869dc276195080c
Bart <bartek.durys@gmail.com>
2017-12-02 17:22:51 +0300

    Explosion asset

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
     public float speed = 0.1f;
     public float range = 5.0f;
+    public float time = 0.0f;
     private GameObject ship;
+    public GameObject explosion;
+    MeshRenderer MeshComponent;
     // Use this for initialization
     void Start () {
         ship = GameObject.FindGameObjectWithTag("Ship");
@@ -13,15 +16,27 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        MeshComponent= gameObject.GetComponent<MeshRenderer>();
         transform.Translate(speed,0,0);
         if(Vector3.Distance(transform.position,ship.transform.position)<=range)
         {
             Blow();
         }
+
+        if(speed==0)
+        {
+            time += Time.deltaTime;
+            if(time>7)
+            {
+                Destroy(gameObject);
+            }
+        }
 	}
 
     void Blow()
     {
-        Destroy(gameObject);
+        MeshComponent.GetComponent<Renderer>().enabled = false;
+        speed = 0;
+        explosion.SetActive(true);
     }
 }
