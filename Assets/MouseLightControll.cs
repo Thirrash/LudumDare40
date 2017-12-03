8c55e94740d2aca324968c2dff8561348ab3d477
Latawiec <lmarzec08@gmail.com>
2017-12-03 05:37:15 +0100

    Added mouse controll to submarine light

---
+++ Assets/MouseLightControll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLightControll : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100000, 1 << 10))
+        {
+            transform.forward = hit.point - transform.position;
+        }
+
+    }
+}
