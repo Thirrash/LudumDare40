975050c56ef1c81a368966ac61aafbeb5b1bd1d7
Thirr-Lap <thirrasch@gmail.com>
2017-12-03 18:47:19 +0100

    no message

--- Assets/Scripts/UI/Intro.cs
+++ Assets/Scripts/UI/Intro.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class Intro : MonoBehaviour
 {
-
-
     // Use this for initialization
     void Start() {
-
+        Time.timeScale = 0.0f;
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Q)) {
-
-
-
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Time.timeScale = 1.0f;
             gameObject.SetActive(false);
         }
     }
